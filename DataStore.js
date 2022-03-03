b3737ebcf55ffc143cb8523125d99b1b82295206
AriParvin <arian.parvin@parvin.de>
2022-03-03 13:32:50 +0100

    added class DataStore in DataStore.js

--- DataStore.js
+++ DataStore.js
@@ -1,9 +1,37 @@
-const Store = require('electron-store')
+const Store = require('electron-store');
 
 class DataStore extends Store {
-	constructor (settings) {
-		//same as New Store(settings)
-		super(settings)
+    constructor(settings) {
+        //same as New Store(settings)
+        super(settings);
 
-		//initialize with todos or empty array
-		//this.todos = this.get 
+        //initialize with todos or empty array
+        this.todos = this.get('todos') || [];
+    }
+    saveTodos() {
+        //save todos to JSON File
+        this.set('todos', this.todos);
+
+        //returning 'this' allows method chaining
+        return this;
+    }
+
+    getTodos() {
+        // set object's todos to todos in JSON file
+        this.todos = this.get('todos') || [];
+
+        return this;
+    }
+    addTodo(todo) {
+        //merge existing todos with new todo
+        this.todos = [...this.todos, todo];
+        return this.saveTodos();
+    }
+
+    deleteTodo(todo) {
+        //filter out target todo
+        this.todos = this.todos.filter((t) => t !== todo);
+        return this.saveTodos();
+    }
+}
+module.exports = DataStore;
